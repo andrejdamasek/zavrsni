274ba36c6304d756a85e33f5f5e42466891d4344
andrejdamasek <150904295+andrejdamasek@users.noreply.github.com>
2025-04-29 15:44:47 +0200

    Updates

--- program.py
+++ program.py
@@ -2,7 +2,7 @@
 import rospy
 import numpy as np
 import cv2
-from core.real_ur5_controller import UR5Controller  # Assuming your class is in this file
+from core.real_ur5_controller import UR5Controller  
 
 points = np.empty((0, 2), dtype=int) 
 is_drawing = False
@@ -29,19 +29,42 @@ def draw(event, x, y, flags, param):
     global is_drawing, points
     if event == cv2.EVENT_LBUTTONDOWN:
         is_drawing = True
-        points = np.vstack((points, [-1, -1]))
+        if points.shape[0] > 0: 
+            points = np.vstack((points, [-1, -1]))
         points = np.vstack((points, [x, y]))
+
     elif event == cv2.EVENT_MOUSEMOVE and is_drawing:
         points = np.vstack((points, [x, y]))
+
     elif event == cv2.EVENT_LBUTTONUP:
         is_drawing = False
 
-
-
+def sample_between_none(points, step=5):
+    result = []
+    segment = []
     
-def transform_to_3d(scale=0.001, z_height=0, offsets=(-0.4, 0.5, 0)):
+    for point in points:
+        if np.array_equal(point, [-1, -1]):
+            if segment:
+                sampled = segment[::step]
+                if not np.array_equal(segment[-1], sampled[-1]):
+                    sampled.append(segment[-1])  
+                result.extend(sampled)
+                segment = []
+            result.append(point)
+        else:
+            segment.append(point)
+    
+    if segment:
+        sampled = segment[::step]
+        if not np.array_equal(segment[-1], sampled[-1]):
+            sampled.append(segment[-1])
+        result.extend(sampled)
+
+    return np.array(result)
+    
+def transform_to_3d(points, scale=0.001, z_height=0, offsets=(-0.25, 0.8, 0)):
     X_offset, Y_offset, Z_offset = offsets
-    global points
     points_3d = []
 
     for point in points:
@@ -49,14 +72,37 @@ def transform_to_3d(scale=0.001, z_height=0, offsets=(-0.4, 0.5, 0)):
             points_3d.append(None)
         else:
             x_3d = point[0] * scale  + X_offset
-            y_3d = -point[1] * scale+ Y_offset
+            y_3d = -point[1] * scale + Y_offset
             z_3d = z_height + Z_offset
             points_3d.append((x_3d, y_3d, z_3d))
+
     return points_3d
 
+def send_robot_to_start_position(controller):
+    current_pose = controller.get_current_tool_pose()
+    T_6_0 = np.copy(current_pose)
+    T_6_0[0, 3] = -0.25   # X
+    T_6_0[1, 3] = 0.8   # Y
+    T_6_0[2, 3] = 0.3   # Z
+
+    T_6_0[:3, :3] = np.array([[1, 0, 0],
+                              [0, -1, 0],
+                              [0, 0, -1]])
+
+    start_joints = controller.get_closest_ik_solution(T_6_0)
+
+    if start_joints is not None:
+        traj = [controller.get_current_joint_values(), start_joints]
+        controller.send_joint_trajectory_action(np.array(traj), max_velocity=0.5, max_acceleration=0.5)
+        rospy.sleep(2)
+        print("Robot postavljen u početnu poziciju.")
+    else:
+        rospy.logwarn("Nije pronađeno IK rješenje za početnu poziciju.")
+
+
 def main():
-    global is_drawing 
-    global points
+    global is_drawing, points
+
     while True:
     
         print("Pritisni 'w' za početak crtanja, 'r' za pokretanje robota, 's' za spremanje, 'l' za učitavanje, 'b' za brisanje, 'q' za izlaz.")
@@ -99,85 +145,59 @@ def main():
 
 
         elif key == 'r':
+            
             print("Pokretanje robota.")
             
             rospy.init_node("ur5_simple_demo")
             controller = UR5Controller()
-
             rospy.loginfo("Starting UR5 simple demo...")
 
-            
+            send_robot_to_start_position(controller)
+
+
             current_pose = controller.get_current_tool_pose()
             T_6_0 = np.copy(current_pose)
 
-            # T_6_0[0,3]+=0.1 # move +10 cm in X direction
-            # T_6_0[2, 3] += 0.10  # move +10 cm in Z direction
+            z_up = 0.05 
+            pen_is_down = False  
+            T_6_0[:3, :3] = np.array([[1, 0, 0],
+                                     [0, -1, 0],
+                                     [0, 0, -1]])
 
-            # rospy.loginfo("Planning Cartesian move 10 cm in X and Z...")
-            # joint_sol = controller.get_closest_ik_solution(T_6_0)
-            #joint_sol[0] += np.pi
-            #joint_sol[joint_sol>np.pi]-=(2.0*np.pi)
-            #joint_sol[joint_sol<-np.pi]+=(2.0*np.pi)
-            # if joint_sol is not None:
-            #    current_joints = controller.get_current_joint_values()
-            #    joint_traj = np.array([current_joints, joint_sol])
-            #    controller.send_joint_trajectory_action(joint_traj, max_velocity=0.5, max_acceleration=0.5)
-            # else:
-            #    rospy.logwarn("No IK solution found for offset pose.")
-
-            # rospy.sleep(1.0)
-
-            # T_6_0[1,3]+=0.1 # move +10 cm in y direction
-            # T_6_0[2, 3] -= 0.10  # move +10 cm in Z direction
-            # rospy.loginfo("Planning Cartesian move 10 cm in y and -10cm in Z...")
-            # joint_sol = controller.get_closest_ik_solution(T_6_0)
-            #joint_sol[0] += np.pi
-            #joint_sol[joint_sol>np.pi]-=(2.0*np.pi)
-            #joint_sol[joint_sol<-np.pi]+=(2.0*np.pi)
-            # if joint_sol is not None:
-            #    current_joints = controller.get_current_joint_values()
-            #    joint_traj = np.array([current_joints, joint_sol])
-            #    controller.send_joint_trajectory_action(joint_traj, max_velocity=0.5, max_acceleration=0.5)
-            # else:
-            #    rospy.logwarn("No IK solution found for offset pose.")
-
-            # rospy.sleep(1.0)
-            #controller.shutdown()
-            
-            points_3d=transform_to_3d()
+           
+            new_points = sample_between_none(points, step=5)
+            points_3d=transform_to_3d(new_points)
+            print("Pokretanje robota.")
             current_joints = controller.get_current_joint_values()
             traj = [current_joints]
-            for point in points_3d[::5]:
-                if point is not None:
-                    T_6_0[:3, :3] = np.array([[1, 0, 0],
-                                              [0, -1, 0],
-                                              [0, 0, -1]])
-                    #T_6_0[:3,3]=point
-                    T_6_0[0, 3] = point[0]                
+
+           
+            for point in points_3d:
+                if point is None:
+                    if pen_is_down:
+                        T_6_0[2, 3] += z_up 
+                        joint_sol = controller.get_closest_ik_solution(T_6_0)
+                        if joint_sol is not None:
+                            traj.append(joint_sol.tolist())
+                        else:
+                            rospy.logwarn("No IK solution found for offset pose.")
+                        pen_is_down = False
+
+                else:                    
+                    T_6_0[0, 3] = point[0]
                     T_6_0[1, 3] = point[1]
+                    if not pen_is_down:
+                        T_6_0[2, 3] -= z_up 
+                        pen_is_down = True
                     
-                    rospy.loginfo("Planning Cartesian move...")
                     joint_sol = controller.get_closest_ik_solution(T_6_0)
                     if joint_sol is not None:
-                        # joint_traj = np.array([current_joints, joint_sol])
                         traj.append(joint_sol.tolist())
-                        # controller.send_joint_trajectory_action(joint_traj, max_velocity=0.5, max_acceleration=0.5)
-                    else:
-                        rospy.logwarn("No IK solution found for offset pose.")
-                    rospy.sleep(1.0)
-                    
-                # else:
-                #     T_6_0[2,3]+=0.02
-                #     rospy.loginfo("Planning Cartesian move...")
-                #     joint_sol = controller.get_closest_ik_solution(T_6_0)
-                #     if joint_sol is not None:
-                #         current_joints = controller.get_current_joint_values()
-                #         joint_traj = np.array([current_joints, joint_sol])
-                #         controller.send_joint_trajectory_action(joint_traj, max_velocity=0.5, max_acceleration=0.5)
+
+                rospy.sleep(1)
             
             controller.send_joint_trajectory_action(np.array(traj), max_velocity=0.5, max_acceleration=0.5)
-            controller.shutdown()
-                      
+            controller.shutdown()                     
         
 
 if __name__ == "__main__":
